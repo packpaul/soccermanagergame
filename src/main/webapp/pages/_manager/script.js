f1a82acc3d90ca6aa4f5bccec0d0674d6b48e112
Pavel Perminov <packpaul@mail.ru>
2018-09-20 22:48:35 +0300

    Dynamic page loading added to the main page

--- src/main/webapp/pages/_manager/script.js
+++ src/main/webapp/pages/_manager/script.js
@@ -1,20 +1,98 @@
 'use strict';
 
-$.Manager = {};
+$.Manager = {
 
-$.Manager.doLoadPage = function(page) {
-    // TODO:
-}
+    doLogout: function() {
+        if (! confirm('Do you really want to log-out?')) {
+            return;
+        }
+        
+        var callback = function() {
+            window.location.replace('/login.html');  
+        };
+        
+        $.rest.logout(callback, callback);
+    }
 
-$.Manager.doLogout = function() {
+};
+
+
+(function() {
+
+    var pageHandlers = {};
+    var currentPage;
     
-    if (! confirm('Do you really want to log-out?')) {
-        return;
+    // "page" loader
+    function app(pageName, param) {
+    
+        var $page = $(document.body).find("section.content#" + pageName);  
+      
+        var src = $page.attr("src");
+        if (src && ($page.find(">:first-child").length == 0)) {
+            // it has src and is empty - load it
+            $.get(src, "html")
+                .done(function(html) {
+                    currentPage = pageName;
+                    $page.html(html);
+                    show(pageName, param);
+                 })
+                .fail(function() {
+                    $page.html("failed to get " + src);
+                    show(pageName, param);
+                 });
+        } else {
+            show(pageName, param);
+        }
     }
     
-    var callback = function() {
-        window.location.replace('/login.html');  
-    };
+    // show the "page" with optional parameter
+    function show(pageName, param) { 
+        // invoke page handler
+        var ph = pageHandlers[pageName]; 
+        if (ph) { 
+            var $page = $("section.content#" + pageName);
+            ph.call($page.length ? $page[0] : null, param); // call "page" handler
+        }
+
+        // activate the page  
+        $("ul.sidebar-menu").find("li").removeClass("active");
+        $("ul.sidebar-menu").find("a[href='#" + pageName + "']").closest("li").addClass("active");
+        // TODO: enable upper level in tree menus
+      
+        $(document.body).attr("page", pageName)
+                        .find("section.content").addClass("hidden")
+                        .filter("section.content#" + pageName).removeClass("hidden");
+    }
+
+    // register page handler  
+    app.handler = function(handler) {
+        var $page = $(document.body).find("section.content#" + currentPage);  
+        pageHandlers[currentPage] = handler.call($page[0]);
+    }
     
-    $.rest.logout(callback, callback);
-}
+    function onhashchange() {
+        var hash;
+        var param;
+        
+        if (location.hash) {
+            var rexp = /#([-_0-9A-Za-z]+)(\:(.+))?/;
+            var match = rexp.exec(location.hash);
+            hash = match[1];
+            param = match[3];
+        } else {
+            hash = $("section.content")[0].id;
+        }
+
+        app(hash, param); // navigate to the page
+    }
+    
+    window.onhashchange = onhashchange; // attach hashchange handler
+    
+    $.Manager.app = app; // setup the app as global object
+    
+    $(function() {
+        // initial state setup
+        window.onhashchange();
+     });
+
+  })();
